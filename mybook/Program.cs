c5b8f998a4a52089626c3a8da0f6dbcfef6d59d0
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-05-19 09:57:12 +0900

    문제 4번 mybook

--- mybook/Program.cs
+++ mybook/Program.cs
@@ -8,8 +8,35 @@ namespace mybook
 {
     class Program
     {
+        class mybook
+        {
+            public string name;
+            public string first;
+            public string person1;//지은이
+            public string person2;//펴낸이
+            public string edit;
+            public string plan;
+            public string editperson;
+            public string design;
+        }
         static void Main(string[] args)
         {
+            List<mybook> list = new List<mybook>();
+            list.Add(new mybook(){ name = "PHP 프로그래밍 입문",
+                first = "2013년 5월 20일",
+                person1="황재호",
+                person2="김태헌",
+                edit="한빛아카데미(주)",
+                plan="김이화",
+                editperson="김이화",
+                design="여동일"
+            });
+
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.name + item.first + item.person1+ item.person2 + item.edit + item.plan + item.editperson + item.design);
+            }
+            
         }
     }
 }
